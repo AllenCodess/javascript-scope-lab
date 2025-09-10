821d529e12b101711e1c14d16e110f50fb9a46b2
AllenCodess <Aph296@protonmail.com>
2025-09-10 13:28:17 -0400

    working on lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+
+// Step 1: Variable Data
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// add flatburger to the array
+function addBurger() {
+  const newBurger = 'Flatburger'; // function-scoped
+  burgers.push(newBurger);
+}
+
+// if true add maple bacon burger to the array
+if (true) {
+  let anotherNewBurger = 'Maple Bacon Burger';
+  burgers.push(anotherNewBurger);
+}
+
+// changeFeaturedDrink changes the value of feturedDrink
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';
+}
+
+
+
+  
